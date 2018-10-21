0d225c11c666b3c69e05bea3c0d502ae684f6a37
Rafael Freire <rafaelfreireweb@gmail.com>
2018-10-21 19:44:59 -0300

    Updates

---
+++ precache-manifest.1b78e14f8171e239da9977506e1c7c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b93c2ad436ccb747ae415c1e865819a4",
+    "url": "/react-calculator/static/media/RobotoMono-Thin.b93c2ad4.ttf"
+  },
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "38d5b871ca7ec7a3b6d0",
+    "url": "/react-calculator/static/js/main.38d5b871.chunk.js"
+  },
+  {
+    "revision": "fbea42c90aafa286c288",
+    "url": "/react-calculator/static/js/1.fbea42c9.chunk.js"
+  },
+  {
+    "revision": "38d5b871ca7ec7a3b6d0",
+    "url": "/react-calculator/static/css/main.984c97bc.chunk.css"
+  },
+  {
+    "revision": "19891812ac0f6eb7f0a1a24ca8d33e69",
+    "url": "/react-calculator/index.html"
+  }
+];
